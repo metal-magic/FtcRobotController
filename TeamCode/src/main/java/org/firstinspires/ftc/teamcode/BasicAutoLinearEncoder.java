3cb45723a916380f0ef7de53c7093582bccc555b
aerovik <avikjain2212@gmail.com>
2023-06-22 18:30:29 -0400

    v1.0.0
    enabled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutoLinearEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutoLinearEncoder.java
@@ -62,7 +62,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @Autonomous(name="Robot: Auto Drive By Encoder", group="Robot")
-@Disabled
 public class BasicAutoLinearEncoder extends LinearOpMode {
 
     /* Declare OpMode members. */
