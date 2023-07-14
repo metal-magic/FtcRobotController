91406876d5e8e18b81f4be272f96bc5ded87c88a
Adi K <129421654+Technicc10@users.noreply.github.com>
2023-07-14 00:51:13 -0400

    Added telemetry class and edited external hardware
    I added a class for telemetry and I added initIMU to the MetalMagicExternalHardware. There is one error and I am not sure if we need to add more to this or make other changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MetalMagicExternalHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MetalMagicExternalHardware.java
@@ -1,7 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
 public class MetalMagicExternalHardware {
 
+    private IMU imu = null;
+    public void runOpMode() throws InterruptedException {
+        initIMU();
+    }
+
+    private void initIMU() {
+//        BHI260IMU.
+//        IMU.Parameters parameters = new BHI260IMU.Parameters(RevHubOrientationOnRobot.LogoFacingDirection.);
+//        parameters.angleUnit = BHI260IMU.a.DEGREES;
+        imu = hardwareMap.get(BHI260IMU.class, "imu");
+        RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
+
+    }
 
 
 }
