d8606ca92c8588257e89ca5e685126091c503e90
Adi K <129421654+Technicc10@users.noreply.github.com>
2023-07-17 13:54:45 -0400

    Made edits to InitIMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MetalMagicExternalHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MetalMagicExternalHardware.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
 import com.qualcomm.hardware.bosch.BHI260IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -13,18 +15,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 public class MetalMagicExternalHardware {
 
     private IMU imu = null;
-    public void runOpMode() throws InterruptedException {
-        initIMU();
+    public void runOpMode(HardwareMap hardwareMap, Telemetry telemetry) throws InterruptedException {
+
+        initIMU(HardwareMap hardwareMap, Telemetry telemetry);
     }
 
     private void initIMU(HardwareMap hardwareMap, Telemetry telemetry) {
 //        BHI260IMU.
 //        IMU.Parameters parameters = new BHI260IMU.Parameters(RevHubOrientationOnRobot.LogoFacingDirection.);
 //        parameters.angleUnit = BHI260IMU.a.DEGREES;
-        imu = hardwareMap.get(BHI260IMU.class, "imu");
+        imu = hardwareMap.get(IMU.class, "imu");
         RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
+
 
     }
 
