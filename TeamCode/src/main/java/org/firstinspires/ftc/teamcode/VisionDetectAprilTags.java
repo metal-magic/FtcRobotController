65bfe4c18af8fef3ebee40d61abbfc4e06fcb82a
aryanmathur1 <134450794+aryanmathur1@users.noreply.github.com>
2023-10-22 10:55:35 -0400

    Created code to detect April Tags
    Completed Task: Rotate robot until it detects an AprilTag

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionDetectAprilTags.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class VisionDetectAprilTags extends LinearOpMode{
+
+    //stating the motors
+    DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+    DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+    DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+    DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+       //MyDriveTrain m = new MyDriveDrain();
+        //m.rotate(90);
+
+        //drawing information on the driver station camera screen
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        //stating the webcam
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+
+
+
+        waitForStart();
+
+
+        while (!isStopRequested() && opModeIsActive()) {
+
+            //rotate robot until it detects an AprilTag
+            if (tagProcessor.getDetections().size() == 0) {
+                rotateRobot(0.5);
+            } else {
+                rotateRobot(0);
+            }
+
+
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                //sending telemetry values to the driver station
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
+
+            }
+            telemetry.update();
+
+
+        }
+
+    }
+    //method to rotate robot
+    private void rotateRobot(double value) {
+        motorFrontLeft.setPower(value);
+        motorBackLeft.setPower(value);
+        motorFrontRight.setPower(-value);
+        motorBackRight.setPower(-value);
+
+    }
+}
