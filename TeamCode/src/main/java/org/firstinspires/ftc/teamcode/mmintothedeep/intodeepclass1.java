51571e6ddd95e6627a6396b925c2c5a51f73731f
Nik <robo.nik10@gmail.com>
2024-09-11 20:44:00 -0400

    Organizing Software
    Adding metal magic into the deep folder with auto and teleop folders within that, need to organize

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mmintothedeep/intodeepclass1.java
@@ -0,0 +1,4 @@
+//This file is just for package making within the package mmintothedeep
+//I had to make a class within the mmintothedeep package so it would allow me to
+//make other packages
+//IF U DELETE U ARE NOT SKIBIDI KYS
