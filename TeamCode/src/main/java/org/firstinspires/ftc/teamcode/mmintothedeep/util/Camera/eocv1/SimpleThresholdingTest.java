0f7d4d6366298ca49878c641832b39d2d517919f
srinandasai12 <108890484+srinandasai12@users.noreply.github.com>
2024-09-22 18:57:55 -0400

    EOCV Major Update
    Masks input frame from a webcam by keeping only blue pixels or only yellow pixels

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mmintothedeep/util/Camera/eocv1/SimpleThresholdingTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.mmintothedeep.util.Camera.eocv1;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class SimpleThresholdingTest extends OpenCvPipeline {
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Scalar lower_blue = new Scalar(85, 50, 40);
+        Scalar upper_blue = new Scalar(135, 255, 255);
+
+        Scalar lower_red = new Scalar(20, 100, 100);
+        Scalar upper_red = new Scalar(135, 255, 255);
+
+        // Both blue and yellow Scalar lower_red = new Scalar(20, 100, 100);
+        // Both blue and yellow Scalar upper_red = new Scalar(135, 255, 255);
+
+        Scalar lower_yellow = new Scalar(20, 100, 100);
+        Scalar upper_yellow = new Scalar(30, 255, 255);
+
+        //Convert RGB to HSV
+        Mat hsvMat = new Mat();
+        Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
+
+        //Apply Threshold
+        Mat threshold = new Mat();
+        // Core.inRange(hsvMat, lower_blue, upper_blue, threshold);
+        // Core.inRange(hsvMat, lower_red, upper_red, threshold);
+        Core.inRange(hsvMat, lower_yellow, upper_yellow, threshold);
+
+        return threshold;
+    }
+}
