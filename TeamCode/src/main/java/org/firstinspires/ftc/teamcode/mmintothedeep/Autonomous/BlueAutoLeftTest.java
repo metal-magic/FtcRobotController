37271052a769da322ccf85c38428c230a4fab9a1
srinandasai12 <108890484+srinandasai12@users.noreply.github.com>
2024-09-22 17:38:42 -0400

    EOCV Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mmintothedeep/Autonomous/BlueAutoLeftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mmintothedeep/Autonomous/BlueAutoLeftTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.mmintothedeep.Autonomous;
+package org.firstinspires.ftc.teamcode.mmintothedeep.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
