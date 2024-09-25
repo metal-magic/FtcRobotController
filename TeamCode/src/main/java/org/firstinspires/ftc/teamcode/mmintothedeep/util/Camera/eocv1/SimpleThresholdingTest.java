cf40c3f59a0b54406068399de9deefc4deba299c
srinandasai12 <108890484+srinandasai12@users.noreply.github.com>
2024-09-25 19:38:20 -0400

    Fixed red color thresholding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mmintothedeep/util/Camera/eocv1/SimpleThresholdingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mmintothedeep/util/Camera/eocv1/SimpleThresholdingTest.java
@@ -13,15 +13,18 @@ public class SimpleThresholdingTest extends OpenCvPipeline {
         Scalar lower_blue = new Scalar(85, 50, 40);
         Scalar upper_blue = new Scalar(135, 255, 255);
 
-        Scalar lower_red = new Scalar(20, 100, 100);
-        Scalar upper_red = new Scalar(135, 255, 255);
+        Scalar lower_red = new Scalar(0, 125, 80);
+        Scalar upper_red = new Scalar(10, 255, 255);
+
+        // Brown => (40, 20, 90);
+        // Brown => (255, 70, 255);
 
         // Both blue and yellow Scalar lower_red = new Scalar(20, 100, 100);
         // Both blue and yellow Scalar upper_red = new Scalar(135, 255, 255);
 
         Scalar lower_yellow = new Scalar(20, 100, 100);
         Scalar upper_yellow = new Scalar(30, 255, 255);
-
+        // GBR
         //Convert RGB to HSV
         Mat hsvMat = new Mat();
         Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
@@ -29,8 +32,8 @@ public class SimpleThresholdingTest extends OpenCvPipeline {
         //Apply Threshold
         Mat threshold = new Mat();
         // Core.inRange(hsvMat, lower_blue, upper_blue, threshold);
-        // Core.inRange(hsvMat, lower_red, upper_red, threshold);
-        Core.inRange(hsvMat, lower_yellow, upper_yellow, threshold);
+        Core.inRange(hsvMat, lower_red, upper_red, threshold);
+        // Core.inRange(hsvMat, lower_yellow, upper_yellow, threshold);
 
         return threshold;
     }
