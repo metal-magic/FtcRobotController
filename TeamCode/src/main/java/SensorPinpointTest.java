05d170bcfeeb17216af5748b859833d00ebd0be8
Nik <robo.nik10@gmail.com>
2024-11-15 21:06:16 -0500

    DriveBackTest
    Made DriveBackTest, started it but it is unfinished.

--- TeamCode/src/main/java/SensorPinpointTest.java
+++ TeamCode/src/main/java/SensorPinpointTest.java
@@ -82,7 +82,7 @@ public class SensorPinpointTest extends LinearOpMode {
         the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
         backwards is a negative number.
          */
-        odo.setOffsets(-2.0, 98.0); //these are tuned for 3110-0002-0001 Product Insight #1
+        odo.setOffsets(115.0, -2.0); //these are tuned for 3110-0002-0001 Product Insight #1
 
         /*
         Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
