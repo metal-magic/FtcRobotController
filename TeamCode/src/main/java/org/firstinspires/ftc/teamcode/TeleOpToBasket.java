5495a8a7d01a2811436e1a8b48b1259cda3d6887
aryanmathur1 <134450794+aryanmathur1@users.noreply.github.com>
2024-12-22 16:49:37 -0500

    Update TeleOpToBasket.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpToBasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpToBasket.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
