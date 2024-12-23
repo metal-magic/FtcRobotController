ef02842d3b0cc02ca09baad921ec073c30b7a25c
srinandasai12 <108890484+srinandasai12@users.noreply.github.com>
2024-12-22 19:10:40 -0500

    pinpointUpdates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointAutoRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointAutoRight.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -7,24 +9,38 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
 
 import java.util.Locale;
 
-@Autonomous(name="PinpointPathing", group="Pinpoint")
+@Autonomous(name="localizeWithTag", group="Pinpoint")
 //@Disabled
 
-public class PinpointPathing extends LinearOpMode {
+public class PinpointAutoRight extends LinearOpMode {
 
     DcMotor leftFrontDrive;
     DcMotor rightFrontDrive;
     DcMotor leftBackDrive;
     DcMotor rightBackDrive;
 
+    VisionPortal visionPortal;
+    AprilTagProcessor tagProcessor;
+
+    static final double cameraOffsetY = -127.0;
+    static final double cameraOffsetX = -203.2;
+
     GoBildaPinpointDriver odo; // Declare OpMode member for the Odometry Computer
     DriveToPoint nav = new DriveToPoint(this); //OpMode member for the point-to-point navigation class
+
+    //int[] viewIds = VisionPortal.makeMultiPortalView(1, VisionPortal.MultiPortalLayout.VERTICAL);
 
     enum StateMachine {
         WAITING_FOR_START,
@@ -36,25 +52,26 @@ public class PinpointPathing extends LinearOpMode {
         DRIVE_TO_TARGET_5
     }
 
-    static final Pose2D TARGET_0 = new Pose2D(DistanceUnit.MM,707,195,AngleUnit.DEGREES,0);
-    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM,0,0,AngleUnit.DEGREES,0);
+    static Pose2D startingPos = null;
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM, 707, 195, AngleUnit.DEGREES, 0);
     static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, 200, -658, AngleUnit.DEGREES, -138.13);
-    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM,353,428, AngleUnit.DEGREES,90);
-    static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM,304, 1210, AngleUnit.DEGREES,90);
-    static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM,260,1700, AngleUnit.DEGREES,132);
-
-
+    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM, 353, 428, AngleUnit.DEGREES, 90);
+    static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM, 304, 1210, AngleUnit.DEGREES, 90);
+    static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, 260, 1700, AngleUnit.DEGREES, 132);
 
     @Override
     public void runOpMode() {
 
+
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
 
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "motorFrontLeft");
+        initPortal();
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "motorFrontLeft");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "motorFrontRight");
-        leftBackDrive   = hardwareMap.get(DcMotor.class, "motorBackLeft");
-        rightBackDrive  = hardwareMap.get(DcMotor.class, "motorBackRight");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "motorBackLeft");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "motorBackRight");
 
         leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -64,12 +81,14 @@ public class PinpointPathing extends LinearOpMode {
         leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
         leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
         odo.setOffsets(0, -180.0); //these are tuned for 3110-0002-0001 Product Insight #1
         odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
         odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
         odo.resetPosAndIMU();
+
+
 
         //nav.setXYCoefficients(0.02,0.002,0.0,DistanceUnit.MM,12);
         //nav.setYawCoefficients(1,0,0.0, AngleUnit.DEGREES,2);
@@ -83,6 +102,10 @@ public class PinpointPathing extends LinearOpMode {
         telemetry.addData("Y offset", odo.getYOffset());
         telemetry.addData("Device Version Number:", odo.getDeviceVersion());
         telemetry.addData("Device Scalar", odo.getYawScalar());
+
+        telemetry.addData("x", odo.getPosX());
+        telemetry.addData("y", odo.getPosY());
+
         telemetry.update();
 
         boolean attarget = false;
@@ -94,10 +117,36 @@ public class PinpointPathing extends LinearOpMode {
         while (opModeIsActive()) {
             odo.update();
 
-            switch (stateMachine){
+            if (!tagProcessor.getDetections().isEmpty()) {
+                double R = tagProcessor.getDetections().get(0).ftcPose.range;
+                double H = tagProcessor.getDetections().get(0).ftcPose.yaw;
+
+                double Y = Math.cos(Math.toRadians(H)) * R;
+                double X = tagProcessor.getDetections().get(0).ftcPose.x;
+
+
+                telemetry.addData("aprilX", X);
+                telemetry.addData("aprilY", Y);
+                telemetry.addData("aprilY", H);
+                //double lX = Y*Math.tan();
+
+                double lX = (Math.cos(Math.toRadians(H))*X)+24;
+                double lY = (Math.cos(Math.toRadians(H))*Y);
+                double lH = -90+H;
+
+                //lX = 24+tagProcessor.getDetections().get(0).ftcPose.x;
+
+                startingPos = new Pose2D(DistanceUnit.INCH, lY, lX-5, AngleUnit.DEGREES, lH);
+
+                odo.setPosition(startingPos);
+            }
+
+
+
+            switch (stateMachine) {
                 case WAITING_FOR_START:
                     //the first step in the autonomous
-                    stateMachine = StateMachine.DRIVE_TO_TARGET_1;
+                    stateMachine = StateMachine.AT_TARGET;
                     break;
                 case DRIVE_TO_TARGET_1:
                     /*
@@ -105,26 +154,26 @@ public class PinpointPathing extends LinearOpMode {
                     the robot has reached the target, and has been there for (holdTime) seconds.
                     Once driveTo returns true, it prints a telemetry line and moves the state machine forward.
                      */
-                    if (nav.driveTo(odo.getPosition(), TARGET_1, 0.4, 0)){
+                    if (nav.driveTo(odo.getPosition(), TARGET_1, 0.4, 0)) {
                         telemetry.addLine("at position #1!");
-                        stateMachine = StateMachine.DRIVE_TO_TARGET_1;
+                        stateMachine = StateMachine.AT_TARGET;
                     }
                     break;
                 case DRIVE_TO_TARGET_2:
                     //drive to the second target
-                    if (nav.driveTo(odo.getPosition(), TARGET_2, 0.4, 2)){
+                    if (nav.driveTo(odo.getPosition(), TARGET_2, 0.4, 2)) {
                         telemetry.addLine("at position #2!");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_3;
                     }
                     break;
 
                 case DRIVE_TO_TARGET_3:
-                    if(nav.driveTo(odo.getPosition(), TARGET_3, 0.7, 2)){
+                    if (nav.driveTo(odo.getPosition(), TARGET_3, 0.7, 2)) {
                         telemetry.addLine("at position #3");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_4;
-                        long t= System.currentTimeMillis();
-                        long endTimer = t+800;
-                        while(System.currentTimeMillis() < endTimer) {
+                        long t = System.currentTimeMillis();
+                        long endTimer = t + 800;
+                        while (System.currentTimeMillis() < endTimer) {
                             leftFrontDrive.setPower(1);
                             leftBackDrive.setPower(1);
                             rightBackDrive.setPower(-1);
@@ -133,13 +182,13 @@ public class PinpointPathing extends LinearOpMode {
                     }
                     break;
                 case DRIVE_TO_TARGET_4:
-                    if(nav.driveTo(odo.getPosition(),TARGET_4,0.7,0.1)){
+                    if (nav.driveTo(odo.getPosition(), TARGET_4, 0.7, 0.1)) {
                         telemetry.addLine("at position #4");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_5;
                     }
                     break;
                 case DRIVE_TO_TARGET_5:
-                    if(nav.driveTo(odo.getPosition(),TARGET_5,0.7,0)){
+                    if (nav.driveTo(odo.getPosition(), TARGET_5, 0.7, 0)) {
                         telemetry.addLine("There!");
                         stateMachine = StateMachine.AT_TARGET;
                     }
@@ -162,7 +211,7 @@ public class PinpointPathing extends LinearOpMode {
                 rightBackDrive.setPower(nav.getMotorPower(DriveToPoint.DriveMotor.RIGHT_BACK));
             }
 
-            telemetry.addData("current state:",stateMachine);
+            telemetry.addData("current state:", stateMachine);
 
             Pose2D pos = odo.getPosition();
             String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
@@ -171,4 +220,39 @@ public class PinpointPathing extends LinearOpMode {
             telemetry.update();
 
         }
-    }}
+    }
+
+    public void initPortal() {
+
+        // Because we want to show two camera feeds simultaneously, we need to inform
+        // the SDK that we want it to split the camera monitor area into two smaller
+        // areas for us. It will then give us View IDs which we can pass to the
+        // individual
+        // vision portals to allow them to properly hook into the UI in tandem.
+
+        // We extract the two view IDs from the array to make our lives a little easier
+        // later.
+        // NB: the array is 2 long because we asked for 3 portals up above.
+        //int portal1ViewId = viewIds[0];
+
+        // drawing information on the driver station camera screen
+        tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setLensIntrinsics(513.474, 513.474, 316.919, 249.760)
+                .build();
+
+        // stating the webcam
+        visionPortal = new VisionPortal.Builder()
+                //.setLiveViewContainerId(portal1ViewId)
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "tagCam"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+    }
+
+
+}
